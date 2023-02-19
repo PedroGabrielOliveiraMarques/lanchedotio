32cc1cbb0f8eb26243cf592ec531a02b7e53d833
PedroGabrielOliveiraMarques <pg100526@gmail.com>
2023-02-19 14:27:26 -0300

    make it better

--- api.py
+++ api.py
@@ -1,9 +1,4 @@
 from flask import Flask, render_template,request,redirect,flash,url_for
-import json
-from nome.pp import LoginForm
-from pathlib import Path
-import os
-
 
 app = Flask(__name__, template_folder = '../carros')
 app.config['SECRET_KEY']="palavra_secreta123"
